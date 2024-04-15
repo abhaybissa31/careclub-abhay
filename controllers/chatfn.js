ed147c4a7fa2d5c54e640a72567da0c798b7821f
Abhay <theabhaybissa@gmail.com>
2024-04-16 01:31:55 +0530

    chat done 50%

--- controllers/chatfn.js
+++ controllers/chatfn.js
@@ -2,34 +2,95 @@ const chatModel=require('../models/chat');
 const express = require("express");
 const bodyParser = require("body-parser");
 const userData = require('../models/userData');
+const mongoose=require('mongoose');
 const app = express();
 app.use(bodyParser.json());
 
-const getChat= async (req,res)=>{
-    // const messages = chatModel.find({ sender_id: req.session.user._id }, 'msg');
-    
-    // createdUser.forEach(createdUser => {
-    //     console.log(createdUser);
-    // });
-    if (req.session && req.session.user && req.session.user._id) {
-        let messages = await chatModel.find({ sender_id: req.session.user._id });
-        console.log(messages)
-        // messages.forEach(message => {
-        //     console.log(message.msg,'reciver id-', message.receiver_id);
-        // });
-        // console.log('createduser sendind')
-        // ---------------------------------below code for auto suggestions ----------------------------
-        const createdUser = await userData.find({}, { u_name: 1, u_email: 1, _id: 1 });
-       const temp = createdUser.map((item)=> item.u_name);
-       console.log(temp)
-        // console.log(typeof(createdUserUname))
-        res.render('chat',{createdUser:createdUser,createdUname:temp,user:req.session.user._id});
-    } else {
-        // user not logged in action guys
-        res.write(`<html><script>alert('Please login in order to continue'); location.href='/login'</script></html>`)
+
+const getChat = async (req, res) => {
+    // Ensure the user is logged in
+    if (!req.session || !req.session.user || !req.session.user._id) {
+        res.write(`<html><script>alert('Please login in order to continue'); location.href='/login'</script></html>`);
+        return;
     }
-   
-}
+
+    try {
+        const userId = req.session.user._id;
+    
+        // Validate the session user ID
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            console.error(`Invalid ObjectId for session user: ${userId}`);
+            res.status(400).send("Invalid user ID.");
+            return;
+        }
+
+        // Retrieve all chat details where the current user is either sender or receiver
+        const chatDetails = await chatModel.find({
+            $or: [
+                { sender_id: userId },
+                { receiver_id: userId }
+            ]
+        });
+        // console.log(chatDetails)
+        const uniqueUserIds = new Set();
+
+        // Aggregate unique user IDs from the chat details
+        chatDetails.forEach(chat => {
+            uniqueUserIds.add(chat.sender_id.toString());
+            uniqueUserIds.add(chat.receiver_id.toString());
+        });
+
+        const messagesWithSenderReceiver = [];
+
+        for (const id of uniqueUserIds) {
+            if (!mongoose.Types.ObjectId.isValid(id) || id === userId) {
+                // Skip adding current user's details
+                continue;
+            }
+
+            const userData1 = await userData.findById(id);
+
+            if (!userData1) {
+                console.error(`User data not found for user ID: ${id}`);
+                continue;
+            }
+
+            // Find the last message between the logged-in user and the other user
+            const lastMessage = await chatModel.findOne({
+                $or: [
+                    { sender_id: userId, receiver_id: id },
+                    { sender_id: id, receiver_id: userId }
+                ]
+            }).sort({ createdAt: -1 });
+
+            // Construct the message object with user details and the last message
+            const messageObj = {
+                user_id: id,
+                user_name: userData1.u_name,
+                user_Image_URL: userData1.Image_URL,
+                last_message: lastMessage ? lastMessage.msg : null
+            };
+
+            messagesWithSenderReceiver.push(messageObj);
+        }
+
+        // Optionally find all users for some user listing feature
+        const createdUser = await userData.find({}, { u_name: 1, u_email: 1, _id: 1 });
+        const createdUname = createdUser.map(item => item.u_name);
+
+        // Render the chat page with all necessary data
+        res.render('chat', {
+            createdUser,
+            createdUname,
+            user: userId,
+            chatDetails: messagesWithSenderReceiver
+        });
+    } catch (error) {
+        console.error("Error fetching chat details:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
 
 const getRecentMessages = async(req,res)=>{
 
